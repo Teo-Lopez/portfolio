0e2a2660171ff5aa207d9d2735d8cae3ea78f8df
Teo <emaildeteo@gmail.com>
2021-04-28 09:43:21 +0200

    added autoscroll effect

--- client/src/components/pages/Index/Hero.jsx
+++ client/src/components/pages/Index/Hero.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import { useEffect } from 'react'
 import { TechCard } from '../../shared/Card/Cards'
 import jsLogo from './assets/javascript-logo.png'
 import mongoLogo from './assets/mongo-logo.png'
 import nodeLogo from './assets/node-logo.png'
 import reactLogo from './assets/react-logo.png'
 import styled from 'styled-components'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 
 const Header = styled.header`
 	height: calc(100vh - 65px);
@@ -15,7 +15,6 @@ const Header = styled.header`
 	z-index: -1000;
 	text-align: center;
 	color: rgba(250, 250, 250, 0.9);
-
 
 	h1 {
 		font-size: 5em;
@@ -47,16 +46,21 @@ const Header = styled.header`
 const Technologies = styled(Row)``
 
 const Hero = () => {
+	useEffect(() => {
+		setTimeout(() => {
+			window.scrollTo({ top: window.innerHeight, behavior: 'smooth' })
+		}, 3000)
+	}, [])
+
 	return (
 		<Header>
 			<div className='center'>
 				<h1>Teodoro López</h1>
 				<Technologies className='justify-content-around align-items-center'>
-					
 					<Col className='col-xl-2 col-lg-3 col-md-4 col-sm-6 col-12'>
 						<TechCard imgUrl={reactLogo} name={'React'} />
 					</Col>
-					
+
 					<Col className='col-xl-2 col-lg-3 col-md-4 col-sm-6 col-12'>
 						<TechCard imgUrl={mongoLogo} name={'Mongo'} />
 					</Col>
