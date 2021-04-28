96938efa39d957ec450cae4053faaa8ec2a0ea1a
Teo <emaildeteo@gmail.com>
2021-04-28 09:45:48 +0200

    added info to projects

--- client/src/components/pages/Index/Projects.jsx
+++ client/src/components/pages/Index/Projects.jsx
@@ -5,13 +5,20 @@ import lab from './assets/lab.jpg'
 import TrackVisibility from 'react-on-screen'
 import ProjectCard from './ProjectCard'
 
+const ProjectsWrapper = styled.section`
+	text-align: center;
+	width: 100%;
+	h2 {
+		padding: 30px;
+	}
+`
+
 const StyledGrid = styled.div`
 	display: grid;
 	background-color: rgb(250, 250, 250);
 	grid-template-columns: repeat(4, 1fr);
 	grid-auto-rows: minmax(300px, auto);
 	grid-gap: 1px;
-	padding: 30px;
 
 	.left-col {
 		grid-column: 1 / 3;
@@ -35,35 +42,41 @@ const StyledGrid = styled.div`
 
 function Projects() {
 	return (
-		<StyledGrid>
-			<TrackVisibility once partialVisibility className='left-col'>
-				<ProjectCard
-					direction='left'
-					img={dice}
-					description={
-						'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto voluptates ea quis voluptate ipsum cupiditate qui omnis tota blanditiis possimus voluptatem quod, quisquam optio rerum fuga facilis est repellendus sed!'
-					}
-				/>
-			</TrackVisibility>
-			<TrackVisibility once partialVisibility className='first-row'>
-				<ProjectCard
-					direction='right'
-					img={lab}
-					description={
-						'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto voluptates ea quis voluptate ipsum cupiditate qui omnis tota blanditiis possimus voluptatem quod, quisquam optio rerum fuga facilis est repellendus sed!'
-					}
-				/>
-			</TrackVisibility>
-			<TrackVisibility once partialVisibility className='second-row'>
-				<ProjectCard
-					direction='right'
-					img={echodot}
-					description={
-						'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iusto voluptates ea quis voluptate ipsum cupiditate qui omnis tota blanditiis possimus voluptatem quod, quisquam optio rerum fuga facilis est repellendus sed!'
-					}
-				/>
-			</TrackVisibility>
-		</StyledGrid>
+		<ProjectsWrapper>
+			<h2>Proyectos</h2>
+			<StyledGrid id='projects'>
+				<TrackVisibility once partialVisibility className='left-col'>
+					<ProjectCard
+						direction='left'
+						title='TellMaster'
+						img={dice}
+						url='https://tellmaster.herokuapp.com/'
+						description={
+							'Aplicación web desarrollada en React para crear y jugar historias interactivas. Crea un personaje, decide tú historia y sumergete en el mundo del rol.'
+						}
+						techs={['React', 'Node', 'Express', 'MongoDB', 'CKeditor', 'Hooks & Context']}
+					/>
+				</TrackVisibility>
+				<TrackVisibility once partialVisibility className='first-row'>
+					<ProjectCard
+						direction='right'
+						img={lab}
+						title='HeroHeart'
+						description={'Web de consulta de diagnosticos médicos realizada con HSB, Node.js y la api de '}
+						techs={['HBS o handlebars', 'Node', 'Express', 'MongoDB', 'Axios']}
+					/>
+				</TrackVisibility>
+				<TrackVisibility once partialVisibility className='second-row'>
+					<ProjectCard
+						direction='right'
+						img={echodot}
+						title='Nueva aventura'
+						description={'Skill de Alexa desarrollada durante 2 semanas.'}
+						techs={['Amazon polly', 'Mongodb', 'Express', 'Amazon dev. console']}
+					/>
+				</TrackVisibility>
+			</StyledGrid>
+		</ProjectsWrapper>
 	)
 }
 
