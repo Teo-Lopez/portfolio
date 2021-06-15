cdc64735ced01d1f41869dc2c35e771e5381f130
Teo <emaildeteo@gmail.com>
2021-06-15 21:50:42 +0200

    added logo

--- client/src/components/layout/Navigation/Navigation.js
+++ client/src/components/layout/Navigation/Navigation.js
@@ -1,44 +1,44 @@
 import { createRef, useEffect, useState } from 'react'
-import { Navbar, NavDropdown } from 'react-bootstrap'
-
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import logo from './teologo.svg'
 import './Navigation.css'
 
 const nav = createRef()
 
 const Navigation = () => {
-	// const [isTransparent, setIsTransparent] = useState(false)
-	// const handleScroll = () => {
-	// 	// if (window.scrollY > window.innerHeight) {
-	// 	// 	setIsTransparent(true)
-	// 	// } else {
-	// 	// 	setIsTransparent(false)
-	// 	// }
-	// }
-	// useEffect(() => {
-	// 	document.addEventListener('scroll', handleScroll)
-	// 	return () => document.removeEventListener('scroll', handleScroll)
-	// }, [])
+  // const [isTransparent, setIsTransparent] = useState(false)
+  // const handleScroll = () => {
+  // 	// if (window.scrollY > window.innerHeight) {
+  // 	// 	setIsTransparent(true)
+  // 	// } else {
+  // 	// 	setIsTransparent(false)
+  // 	// }
+  // }
+  // useEffect(() => {
+  // 	document.addEventListener('scroll', handleScroll)
+  // 	return () => document.removeEventListener('scroll', handleScroll)
+  // }, [])
 
-	return (
-		<Navbar ref={nav} sticky id='navbar' expand='md' fixed='top' variant='dark'>
-			<Navbar.Toggle aria-controls='basic-navbar-nav' />
-			<Navbar.Collapse id='basic-navbar-nav' className='justify-content-around'>
-				<NavDropdown.Item as={'a'} href='#welcome'>
-					Welcome
-				</NavDropdown.Item>
+  return (
+    <Navbar ref={nav} sticky id='navbar' expand='md' fixed='top' variant='dark'>
+      <Navbar.Toggle aria-controls='basic-navbar-nav' />
+      <Navbar.Collapse id='basic-navbar-nav' className='justify-content-around'>
+        <Navbar.Brand href='#home'>
+          <img src={logo} />
+        </Navbar.Brand>
+        <NavDropdown.Item as={'a'} href='#welcome'>
+          Welcome
+        </NavDropdown.Item>
+        <NavDropdown.Item as={'a'} href='#projects'>
+          Projects
+        </NavDropdown.Item>
 
-				<NavDropdown.Item as={'a'} href='#about-me'>
-					About me
-				</NavDropdown.Item>
-				<NavDropdown.Item as={'a'} href='#projects'>
-					Projects
-				</NavDropdown.Item>
-				<NavDropdown.Item as={'a'} href='#contact'>
-					Contact
-				</NavDropdown.Item>
-			</Navbar.Collapse>
-		</Navbar>
-	)
+        <NavDropdown.Item as={'a'} href='#about-me'>
+          About me
+        </NavDropdown.Item>
+      </Navbar.Collapse>
+    </Navbar>
+  )
 }
 
 export default Navigation
