770f09bf390e0912e5c235801fb71da984411fcd
Teo <emaildeteo@gmail.com>
2021-06-15 21:21:08 +0200

    first version

--- client/src/components/pages/Index/ProjectCard.jsx
+++ client/src/components/pages/Index/ProjectCard.jsx
@@ -12,99 +12,113 @@ const appear = keyframes`
 `
 
 const Card = styled.article`
-	width: 100%;
-	background-image: ${({ img }) => `url(${img})`};
-	background-size: contain;
-	background-position: 50%;
-	background-repeat: no-repeat;
-	transition: filter 700ms;
-	transition-delay: 700ms;
-	filter: grayscale(0.8) brightness(1);
-	opacity: 0;
-	animation: 1s ease-out 1 normal forwards ${appear};
+  width: 100%;
+  background-image: ${({ img }) => `url(${img})`};
+  background-size: contain;
+  background-position: 50%;
+  background-repeat: no-repeat;
+  transition: filter 100ms;
+  transition-delay: 100ms;
+  filter: grayscale(0.8) brightness(1);
+  opacity: 0;
+  animation: 1.2s ease-out 1 normal forwards ${appear};
+  cursor: pointer;
 
-	.cover {
-		background-color: rgba(13, 13, 13, 0.7);
-		transition: background-color 700ms;
-	}
-	.underScore {
-		display: inline-block;
-		border-bottom: 1px solid rgba(250, 250, 250, 0.8);
-	}
+  .cover {
+    background-color: rgba(13, 13, 13, 0.7);
+    transition: background-color 100ms;
+  }
+  .underScore {
+    display: inline-block;
+    border-bottom: 1px solid rgba(250, 250, 250, 0.8);
+  }
 
-	.innerWrapper {
-		padding: 16px;
-		color: rgb(250, 250, 250);
-		font-size: 1.8em;
-		width: 100%;
-		text-align: center;
-		opacity: 1;
-		transition: opacity 700ms;
-	}
-	h3 {
-		font-size: 0.7em;
-		letter-spacing: 2px;
-	}
-	p {
-		color: white;
-		font-size: 0.5em;
-		font-weight: 300;
-	}
+  .innerWrapper {
+    padding: 16px;
+    color: rgb(250, 250, 250);
+    font-size: 1.8em;
+    width: 100%;
+    text-align: center;
+    opacity: 1;
+    transition: opacity 100ms;
+  }
+  h3 {
+    font-size: 0.7em;
+    letter-spacing: 2px;
+  }
+  p {
+    color: white;
+    font-size: 0.5em;
+    font-weight: 300;
+  }
 
-	.tech-wrapper {
-		width: 80%;
-		margin: 0 auto;
-		h4 {
-			font-size: 0.5em;
-		}
-	}
+  .tech-wrapper {
+    width: 80%;
+    margin: 0 auto;
+    h4 {
+      font-size: 0.5em;
+    }
+  }
 
-	&.appearRight {
-		transform: translate(400px, 0);
-	}
-	&.appearLeft {
-		transform: translate(-400px, 0);
-	}
+  &.appearRight {
+    transform: translate(400px, 0);
+  }
+  &.appearLeft {
+    transform: translate(-400px, 0);
+  }
 
-	@media only screen and (min-width: 870px) {
-		filter: grayscale(0.4) brightness(0.7);
-		opacity: 1;
+  @media only screen and (min-width: 870px) {
+    opacity: 1;
+    font-size: 1.2em;
 
-		.cover {
-			background-color: initial;
-		}
+    .cover {
+    }
 
-		&:hover {
-			filter: grayscale(0) brightness(1);
+    &:hover {
+      filter: grayscale(0) brightness(1);
 
-			.cover {
-				transition: background-color 700ms;
-				background-color: rgba(13, 13, 13, 0.6);
-			}
-			.innerWrapper {
-				transition: opacity 700ms;
-			}
-		}
-	}
+      .cover {
+        transition: background-color 300ms;
+        background-color: rgba(13, 13, 13, 0.6);
+      }
+      .innerWrapper {
+        transition: opacity 100ms;
+      }
+    }
+  }
 `
 
-function ProjectCard({ img, title, description, isVisible, direction, url, techs = [] }) {
-	const appearClass = direction === 'left' ? 'appearLeft' : 'appearRight'
-	return (
-		<Card img={img} className={isVisible && appearClass}>
-			<a href={url} target='_blank' className='cover' rel='noreferrer'>
-				<div className='innerWrapper'>
-					<h3 className='underScore'>{title}</h3>
-					<p>{description}</p>
-					<hr />
-					<footer className='tech-wrapper'>
-						<h4 className='underScore'>Tecnologías:</h4>
-						<p>{techs.map((elm, idx) => `${elm}${idx === techs.length - 1 ? '.' : ','} `)}</p>
-					</footer>
-				</div>
-			</a>
-		</Card>
-	)
+function ProjectCard({
+  img,
+  title,
+  description,
+  isVisible,
+  direction,
+  url,
+  techs = [],
+}) {
+  const appearClass = direction === 'left' ? 'appearLeft' : 'appearRight'
+  return (
+    <Card img={img} className={isVisible && appearClass}>
+      <div className='readable'>
+        <a href={url} target='_blank' className='cover' rel='noreferrer'>
+          <div className='innerWrapper'>
+            <h3 className='underScore'>{title}</h3>
+            <p>{description}</p>
+            <hr />
+            <footer className='tech-wrapper'>
+              <h4 className='underScore'>Tecnologías:</h4>
+              <p>
+                {techs.map(
+                  (elm, idx) => `${elm}${idx === techs.length - 1 ? '.' : ','} `
+                )}
+              </p>
+            </footer>
+          </div>
+        </a>
+      </div>
+    </Card>
+  )
 }
 
 export default ProjectCard
--- client/src/components/pages/Index/Projects.jsx
+++ client/src/components/pages/Index/Projects.jsx
@@ -7,126 +7,138 @@ import TrackVisibility from 'react-on-screen'
 import ProjectCard from './ProjectCard'
 
 const ProjectsWrapper = styled.section`
-	text-align: center;
-	width: 100%;
-	position: relative;
-	background-color: black;
-	color: white;
+  text-align: center;
+  width: 100%;
+  position: relative;
+  background-color: black;
+  color: white;
 
-	h2 {
-		font-size: 2.5em;
-		letter-spacing: 6px;
-		padding: 30px;
-	}
+  h2 {
+    font-size: 2.5em;
+    letter-spacing: 6px;
+    padding: 30px;
+  }
 `
 
 const StyledGrid = styled.div`
-	display: grid;
-	grid-template-columns: 100%;
-	grid-auto-rows: minmax(300px, auto);
-	grid-gap: 0px;
-	color: white;
+  display: grid;
+  grid-template-columns: 100%;
+  grid-auto-rows: minmax(300px, auto);
+  grid-gap: 0px;
+  color: white;
 
-	.project {
-		display: flex;
-		align-items: center;
-		grid-column: 0 / 1;
-	}
+  .project {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    display: flex;
+    align-items: center;
+    grid-column: 0 / 1;
+  }
 
-	.project:nth-of-type(1) {
-		grid-row: 1 / 2;
-	}
+  .project:nth-of-type(1) {
+    grid-row: 1 / 2;
+  }
 
-	.project:nth-of-type(2) {
-		grid-row: 2 / 3;
-	}
+  .project:nth-of-type(2) {
+    grid-row: 2 / 3;
+  }
 
-	.project:nth-of-type(3) {
-		grid-row: 3 / 4;
-	}
+  .project:nth-of-type(3) {
+    grid-row: 3 / 4;
+  }
 
-	.project:nth-of-type(4) {
-		grid-row: 4 / 5;
-	}
+  .project:nth-of-type(4) {
+    grid-row: 4 / 5;
+  }
 
-	.project:nth-of-type(5) {
-		grid-row: 5 / 6;
-	}
+  @media only screen and (min-width: 870px) {
+    grid-template-columns: 50% 50%;
 
-	@media only screen and (min-width: 870px) {
-		.project:nth-of-type(1) {
-			grid-row: 1 / 2;
-		}
+    .project:nth-of-type(1) {
+      grid-row: 1 / 3;
+    }
 
-		.project:nth-of-type(2) {
-			grid-row: 3 / 4;
-		}
+    .project:nth-of-type(2) {
+      grid-column: 2 /2;
+      grid-row: 1 / 2;
+    }
 
-		.project:nth-of-type(3) {
-			grid-row: 5 / 6;
-		}
+    .project:nth-of-type(3) {
+      grid-column: 1/2;
+      grid-row: 3 / 3;
+    }
 
-		.project:nth-of-type(4) {
-			grid-row: 6 / 7;
-		}
-
-		.project:nth-of-type(5) {
-			grid-row: 8 / 9;
-		}
-	}
+    .project:nth-of-type(4) {
+      grid-column: 2/3;
+      grid-row: 2/4;
+    }
+  }
 `
 
 function Projects() {
-	return (
-		<ProjectsWrapper className='projects'>
-			<h2>Proyectos</h2>
-			<StyledGrid id='projects'>
-				<TrackVisibility once partialVisibility className='project'>
-					<ProjectCard
-						direction='left'
-						title='TellMaster'
-						img={dice}
-						url='https://tellmaster.herokuapp.com/'
-						description={
-							'Aplicación web desarrollada en React para crear y jugar historias interactivas. Crea un personaje, decide tú historia y sumergete en el mundo del rol.'
-						}
-						techs={['React', 'Node', 'Express', 'MongoDB', 'CKeditor', 'Hooks & Context']}
-					/>
-				</TrackVisibility>
-				<TrackVisibility once partialVisibility className='project'>
-					<ProjectCard
-						direction='right'
-						img={lab}
-						title='HeroHeart'
-						url='https://heroheart.herokuapp.com/'
-						description={
-							'Web de consulta de diagnosticos médicos realizada con HSB, Node.js y la api de '
-						}
-						techs={['HBS o handlebars', 'Node', 'Express', 'MongoDB', 'Axios']}
-					/>
-				</TrackVisibility>
-				<TrackVisibility once partialVisibility className='project'>
-					<ProjectCard
-						direction='right'
-						img={echodot}
-						title='Nueva aventura'
-						description={'Skill de Alexa desarrollada durante 2 semanas.'}
-						techs={['Amazon polly', 'Mongodb', 'Express', 'Amazon dev. console']}
-					/>
-				</TrackVisibility>
-				<TrackVisibility once partialVisibility className='project'>
-					<ProjectCard
-						direction='right'
-						img={chronopomo}
-						title='Pomodoro Clock'
-						description={'Web app for Pomodoro inspired time management.'}
-						techs={['React hooks', 'Mongo', 'Styled components']}
-						url='https://chrono-pomo.herokuapp.com/'
-					/>
-				</TrackVisibility>
-			</StyledGrid>
-		</ProjectsWrapper>
-	)
+  return (
+    <ProjectsWrapper className='projects'>
+      <h2>Proyectos</h2>
+      <StyledGrid id='projects'>
+        <TrackVisibility once partialVisibility className='project'>
+          <ProjectCard
+            direction='left'
+            title='TellMaster'
+            img={dice}
+            url='https://tellmaster.herokuapp.com/'
+            description={
+              'Aplicación web desarrollada en React para crear y jugar historias interactivas. Crea un personaje, decide tú historia y sumergete en el mundo del rol.'
+            }
+            techs={[
+              'React',
+              'Node',
+              'Express',
+              'MongoDB',
+              'CKeditor',
+              'Hooks & Context',
+            ]}
+          />
+        </TrackVisibility>
+        <TrackVisibility once partialVisibility className='project'>
+          <ProjectCard
+            direction='right'
+            img={lab}
+            title='HeroHeart'
+            url='https://heroheart.herokuapp.com/'
+            description={
+              'Web de consulta de diagnosticos médicos realizada con HSB, Node.js y la api de '
+            }
+            techs={['HBS o handlebars', 'Node', 'Express', 'MongoDB', 'Axios']}
+          />
+        </TrackVisibility>
+        <TrackVisibility once partialVisibility className='project'>
+          <ProjectCard
+            direction='right'
+            img={echodot}
+            title='Nueva aventura'
+            description={'Skill de Alexa desarrollada durante 2 semanas.'}
+            techs={[
+              'Amazon polly',
+              'Mongodb',
+              'Express',
+              'Amazon dev. console',
+            ]}
+          />
+        </TrackVisibility>
+        <TrackVisibility once partialVisibility className='project'>
+          <ProjectCard
+            direction='right'
+            img={chronopomo}
+            title='Pomodoro Clock'
+            description={'Web app for Pomodoro inspired time management.'}
+            techs={['React hooks', 'Mongo', 'Styled components']}
+            url='https://chrono-pomo.herokuapp.com/'
+          />
+        </TrackVisibility>
+      </StyledGrid>
+    </ProjectsWrapper>
+  )
 }
 
 export default Projects
