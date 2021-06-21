29e39a12becd604a378bfc00c5341c9fe49be9c7
Teo <emaildeteo@gmail.com>
2021-06-21 10:00:09 +0200

    minor changed

--- client/src/components/routes/Routes.js
+++ client/src/components/routes/Routes.js
@@ -1,11 +1,11 @@
 import { Switch, Route } from 'react-router-dom'
 import IndexPage from '../pages/Index/Index'
 
-const Routes = ({ storeUser, loggedUser, isAdmin, handleAlert }) => {
+const Routes = () => {
 	return (
 		<>
 			<Switch>
-				<Route path='/' render={() => <IndexPage />} />
+				<Route exact path='/' render={() => <IndexPage />} />
 			</Switch>
 		</>
 	)
